c4d50e8528cd9cb36476daa28aca6dd756a88471
abdelnaby1 <ahmedabdelnaby471@gmail.com>
2022-02-18 19:33:38 +0200

    lorem-ipsum

---
+++ 8-lorem-ipsum/src/App.js
@@ -0,0 +1,39 @@
+import data from "./data";
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = +count;
+    if (count <= 0) amount = 1;
+    if (count > 8) amount = 8;
+
+    setText(data.slice(0, amount));
+  };
+  return (
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          genrete
+        </button>
+      </form>
+      <article>
+        {text.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </article>
+    </section>
+  );
+}
+
+export default App;
