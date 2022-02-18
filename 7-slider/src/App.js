28bc713a3edae24b1fe39b54f195edad15c5629a
abdelnaby1 <ahmedabdelnaby471@gmail.com>
2022-02-18 14:15:15 +0200

    slider

---
+++ 7-slider/src/App.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import data from "./data";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+
+function App() {
+  const [people, setPeple] = useState(data);
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    if (index < 0) {
+      setIndex(people.length - 1);
+    }
+    if (index > people.length - 1) {
+      setIndex(0);
+    }
+  }, [index, people]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => prev + 1);
+    }, 2500);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [index]);
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, perssonIndex) => {
+          const { id, image, name, title, quote } = person;
+          let position = "nextSlide";
+          if (perssonIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            perssonIndex === index - 1 ||
+            (index === 0 && perssonIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={title} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
