f8540f85fc13655c25cb6049fdb9afc0e88876d4
abdelnaby1 <ahmedabdelnaby471@gmail.com>
2022-04-01 17:50:34 +0200

    stripe submenu

---
+++ 13-stripe/src/App.js
@@ -0,0 +1,17 @@
+import Hero from "./Hero";
+import Navbar from "./Navabar";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
+  );
+}
+
+export default App;
